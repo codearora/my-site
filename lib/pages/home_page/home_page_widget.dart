ca2f0ef5f69ca3e0096a90fe546540e50b01c3ac
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2023-09-13 10:45:54 +0000

    Updating to latest FlutterFlow output.

--- lib/pages/home_page/home_page_widget.dart
+++ lib/pages/home_page/home_page_widget.dart
@@ -44,7 +44,7 @@ class _HomePageWidgetState extends State<HomePageWidget> {
           backgroundColor: FlutterFlowTheme.of(context).primary,
           automaticallyImplyLeading: false,
           title: Text(
-            'Hello World',
+            'Hello World2',
             style: FlutterFlowTheme.of(context).headlineMedium.override(
                   fontFamily: 'Outfit',
                   color: Colors.white,
